365e7a1e1e3746be21c8b5c4e4733d5631f0c6ed
OsvaldoNeto <osvaldo.neto@signus.ind.br>
2024-08-06 15:11:06 -0300

    Aula 02.06 - Estilizando

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -4,7 +4,7 @@ import Logo from './components/Logo/Logo.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App__header">
         <Logo />
       </header>
     </div>
