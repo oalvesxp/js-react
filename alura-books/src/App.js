4b467cb5a826f1d115f3db09f02c6996e6519170
OsvaldoNeto <osvaldo.neto@signus.ind.br>
2024-08-06 15:57:48 -0300

    Aula 02.07 - Usando loops

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
-import Logo from './components/Logo/Logo.js';
+import Logo from './components/Logo/Logo.js'
+import profile from './images/perfil.svg'
+import bag from './images/sacola.svg'
+
+const menuItens = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icons = [profile, bag]
 
 function App() {
   return (
     <div className="App">
-      <header className="App__header">
-        <Logo />
+      <header className='App__header'>
+        <Logo></Logo>
+        <ul className="menu">
+          { menuItens.map((item) => (
+              <li className="menu__item"><p>{item}</p></li>
+          )) }
+        </ul>
+
+        <ul className="menu__icon">
+          { icons.map( (item) => (
+            <li className="menu__icon-item">
+              <img src={item} alt="icon" />
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
