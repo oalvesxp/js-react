89edd70409c6d6f80821f73ecdc966f147f1cc28
OsvaldoNeto <osvaldo.neto@signus.ind.br>
2024-08-06 14:53:17 -0300

    Aula 02.03 - Primeiro componente

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Alura
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sobre
-        </a>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <p>Alura Books</p>
+        </div>
       </header>
     </div>
   );
